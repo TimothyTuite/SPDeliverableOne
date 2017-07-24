20ecce3dd1f38a7679e6acf674a38ddd3de7c4c1
Tim Tuite <n00940915@unf.edu>
2017-07-24 18:35:27 -0400

    first commit

---
+++ SeniorProjectPreReq/SeniorProjectPreReq/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SeniorProjectPreReq.Startup))]
+namespace SeniorProjectPreReq
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
