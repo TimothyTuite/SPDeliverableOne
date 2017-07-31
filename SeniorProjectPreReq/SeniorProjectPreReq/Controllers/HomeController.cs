d1640f11b95d5565c1f7f94de6927d0e11aaa8f2
n00940915 <n00940915@WLIB-64443>
2017-07-31 19:30:20 -0400

    first push

--- SeniorProjectPreReq/SeniorProjectPreReq/Controllers/HomeController.cs
+++ SeniorProjectPreReq/SeniorProjectPreReq/Controllers/HomeController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SeniorProjectPreReq.Models; 
 
 namespace SeniorProjectPreReq.Controllers
 {
+    [HandleError]
     public class HomeController : Controller
     {
+        
+
+        public HomeController()
+        {
+            
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -26,5 +35,9 @@ namespace SeniorProjectPreReq.Controllers
 
             return View();
         }
+
+       
+
+        
     }
 }
