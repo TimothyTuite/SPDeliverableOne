e5df920d9abc4112a4e938059778f7545b37861e
TheGylander <30420988+TheGylander@users.noreply.github.com>
2017-08-10 14:16:32 -0400

    Added Volunteer Controller and Volunteer Model. Volunteer Model still needs modifying. Volunteer Database has been created. Add "/Volunteers" to URL to view Database.

---
+++ SeniorProjectPreReq/SeniorProjectPreReq/Models/Volunteer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace SeniorProjectPreReq.Models
+{
+    public class Volunteer
+    {
+        public int ID { get; set; }
+
+        
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool PendingApproval { get; set; }
+        public bool approved { get; set; }
+
+    }
+
+    public class VolunteerDBContext : DbContext
+    {
+        public DbSet<Volunteer> Volunteers { get; set; }
+    }
+}
