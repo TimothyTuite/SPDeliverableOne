e476040aab2707b2ff4d1289f6f6c2754be32f45
Jon-Michael Nelson Gyland <jmgyland@gmail.com>
2018-01-22 13:01:03 -0500

    Register form input titles cleaned with Display tags.

--- SeniorProjectPreReq/SeniorProjectPreReq/Models/AccountViewModels.cs
+++ SeniorProjectPreReq/SeniorProjectPreReq/Models/AccountViewModels.cs
@@ -66,24 +66,24 @@ namespace SeniorProjectPreReq.Models
     {
         [Required]
         [EmailAddress]
-        [Display(Name = "Email")]
+        [Display(Name = "Email:")]
         public string Email { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Password:")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Display(Name = "Confirm Password:")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
-
+        [Display(Name = "First Name:")]
         public string FirstName { get; set; }
-
+        [Display(Name = "Last Name:")]
         public string LastName { get; set; }
-
+        [Display(Name = "School:")]
         public string school { get; set; }
     }
 
