e443e2d5b95a64e6fa73ff6aef9cf77b08060f3b
Tuite <n00940915@ospreys.unf.edu>
2018-04-03 01:25:37 -0400

    added google maps window showing the address of the school on the schools Details page

--- SeniorProjectPreReq/SeniorProjectPreReq/App_Start/BundleConfig.cs
+++ SeniorProjectPreReq/SeniorProjectPreReq/App_Start/BundleConfig.cs
@@ -22,7 +22,7 @@ namespace SeniorProjectPreReq
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
-
+            bundles.Add(new StyleBundle("~/Content/css/bootstrap").Include("~/Content/bootstrap.min.css", new CssRewriteUrlTransform()));
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
